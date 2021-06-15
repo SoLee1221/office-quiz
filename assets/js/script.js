e35f4a55f5725d002a1bc24be083dcc0181bd131
SoLee1221 <siqbal1221@icloud.com>
2021-06-15 17:36:39 +0000

    add timer function and scores function for correct and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,60 @@ function submitAnswer() {
     handleAnswer(userAnswer == questions[currentQuestion].answer || true);
 }
 
+function handleAnswer(isCorrect) {
+    let q = questions[currentQuestion];
+
+    if (isCorrect)
+        correctAnswers++;
+    else
+    {
+        incorrectAnswers++;
+        displayAnswer(q.answer);
+    }
+
+    updateScoreText();
+
+    // stop timer
+    if (timers !== null) {
+        clearInterval(timers);
+        timers = null;
+    }
+
+    if (currentQuestion + 1 >= questions.length)
+    {
+        updateLeftQuestionsBar(currentQuestion + 1);
+        currentQuestion++;
+
+        // do something after quiz
+
+        return;
+    }
+
+    // reset timer
+    startTimer(3);
+    
+    // 
+    displayQuestion(questions[++currentQuestion]);
+    updateLeftQuestionsBar(currentQuestion);
+}
+
+function startTimer(seconds) {
+    document.getElementById("timer").innerHTML = seconds;
+    counter = seconds;
+    timers = setInterval(timerFunction, 1000);
+}
+
+function timerFunction() {
+    counter = counter - 1;
+    if (counter < 0) { 
+        document.getElementById("timer").innerHTML = "TIMES UP!!!";
+        clearInterval(timers);
+        timers = null;
+        handleAnswer(false);
+    } else { 
+        document.getElementById("timer").innerHTML = counter;
+    } 
+}
+
+startTimer(3);
+displayQuestion(questions[currentQuestion]);
