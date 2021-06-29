794059b810c09162b22e9efbbd92f0ab83f25fce
SoLee1221 <siqbal1221@icloud.com>
2021-06-29 19:41:48 +0000

    added questions to another javascript file

---
+++ assets/js/question.js
@@ -0,0 +1,62 @@
+var questions = [
+    {
+        question: "Who sits opposite Michael Scott's office?",
+        answer: "Jim Halpert",
+        image: "assets/images/jim.jpg"
+    },
+    {
+        question: "Who does Dwight like at the start of the series?",
+        answer: "Katy Moore",
+        image: "assets/images/katy.jpg"
+    },
+    {
+        question: "Who does Pam really have feelings for at the start of the series in office?",
+        answer: "Jim Halpert",
+        image: "assets/images/jim.jpg"
+    },
+    {
+        question: "Who became second acting regional in Dunder Mifflin?",
+        answer: "Dwight Schrute",
+        image: "assets/images/dwigth.jpg"
+    },
+    {
+        question: "Who does Jim have a good close relationship with throughout the series?",
+        answer: "Pam Beesly",
+        image: "assets/images/pam.jpg"
+    },
+    {
+        question: "Who becomes the second to last Dunder Mifflin branch manager before the series ends?",
+        answer: "Andy Bernard",
+        image: "assets/images/andy.jpg"
+    },
+    {
+        question: "Who does Pam end up marrying?",
+        answer: "Jim Halpert",
+        image: "assets/images/jim.jpg"
+    },
+    {
+        question: "Who accidentally lets off a firearm in the office?",
+        answer: "Dwight Schrute",
+        image: "assets/images/dwigth.jpg"
+    },
+    {
+        question: "Who becomes minority executive from the training programme that was offered?",
+        answer: "Kelly Kapoor",
+        image: "assets/images/kelly.jpg"
+    },
+    {
+        question: "Who was the manager of the warehouse?",
+        answer: "Darryl Philbin",
+        image: "assets/images/dar.jpg"
+    },
+    {
+        question: "Who does Michael Scott dislike the most within the office US first few seasons?",
+        answer: "Toby Flenderson",
+        image: "assets/images/toby.jpg"
+    },
+    {
+        question: "Who eats the most M&Ms during the office olympics?",
+        answer: "Kevin Malone",
+        image: "assets/images/kevin.jpg"
+    }
+];
